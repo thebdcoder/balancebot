01c76a9d8aa82cbdef8ddcd37d6c48a8aa3fdf8e
bdcoder <thebdcoder@gmail.com>
2015-02-28 05:36:36 +0600

    pid and motor controller added

--- 6050/combine.py
+++ 6050/combine.py
@@ -4,6 +4,8 @@ import smbus
 import math
 import time
 from MPU6050 import MPU6050
+from PID import PID
+import motor as MOTOR
 
 gyro_scale = 131.0
 accel_scale = 16384.0
@@ -35,30 +37,76 @@ raw_accX = 0.0
 raw_accY = 0.0
 raw_accZ = 0.0
 
+rate_accX = 0.0
+rate_accY = 0.0
+rate_accZ = 0.0
+
 accAngX = 0.0
 
+CFangleX = 0.0
+CFangleX1 = 0.0
 
-print "{0:.4f} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:.2f}".format( time.time() - now, (last_x), gyro_total_x, (last_x), (last_y), gyro_total_y, (last_y))
+K = 0.98
 
-for i in range(0, int(3.0 / time_diff)):
+FIX = -12.89
+
+#print "{0:.4f} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:.2f}".format( time.time() - now, (last_x), gyro_total_x, (last_x), (last_y), gyro_total_y, (last_y))
+
+def dist(a, b):
+    return math.sqrt((a * a) + (b * b))
+
+
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+
+p=PID(1.0,-0.04,0.0)
+p.setPoint(0.0)
+
+for i in range(0, int(300.0 / time_diff)):
     time.sleep(time_diff - 0.005) 
     
+    sensor.read_raw_data()
+    
     # Gyroscope value Degree Per Second / Scalled Data
-	rate_gyroX = sensor.read_scaled_gyro_x()
-	rate_gyroY = sensor.read_scaled_gyro_y()
-	rate_gyroZ = sensor.read_scaled_gyro_z()
+    rate_gyroX = sensor.read_scaled_gyro_x()
+    rate_gyroY = sensor.read_scaled_gyro_y()
+    rate_gyroZ = sensor.read_scaled_gyro_z()
 	
 	# The angle of the Gyroscope
-	gyroAngleX += rate_gyroX * time_diff 
-	gyroAngleY += rate_gyroY * time_diff 
-	gyroAngleZ += rate_gyroZ * time_diff 
+    gyroAngleX += rate_gyroX * time_diff 
+    gyroAngleY += rate_gyroY * time_diff 
+    gyroAngleZ += rate_gyroZ * time_diff 
 	
 	# Accelorometer Value
-	raw_accX = sensor.read_raw_accel_x()
-	raw_accY = sensor.read_raw_accel_y()
-	raw_accZ = sensor.read_raw_accel_z()
-
-	accAngX = ( math.atan2(raw_accY, raw_accZ) + M_PI ) * RAD_TO_DEG
-	
+    raw_accX = sensor.read_raw_accel_x()
+    raw_accY = sensor.read_raw_accel_y()
+    raw_accZ = sensor.read_raw_accel_z()
     
-    print "{0:.4f} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:.2f}".format( time.time() - now, (rotation_x), (gyro_total_x), (last_x), (rotation_y), (gyro_total_y), (last_y))
+    rate_accX = sensor.read_scaled_accel_x()
+    rate_accY = sensor.read_scaled_accel_y()
+    rate_accZ = sensor.read_scaled_accel_z()
+
+    accAngX = ( math.atan2(rate_accX, rate_accY) + M_PI ) * RAD_TO_DEG
+    
+    accAngX1 = get_x_rotation(rate_accX, rate_accY, rate_accX)
+    
+    CFangleX = K * ( CFangleX + rate_gyroX * time_diff) + (1 - K) * accAngX
+    
+    CFangleX1 = ( K * ( CFangleX1 + rate_gyroX * time_diff) + (1 - K) * accAngX1 )
+	
+    pid = p.update(CFangleX1)
+    
+    if(-10 < pid < 10):
+        MOTOR.forward(0)
+    else:
+        if(pid > 10):
+            MOTOR.backward(pid)
+        else:
+            MOTOR.forward(pid*-1)
+    
+    print "{0:.2f} {1:.2f} {2:.2f} {3:.2f} | {4:.2f} {5:.2f} | {6:.2f}".format( gyroAngleX, gyroAngleY , accAngX, CFangleX, accAngX1, CFangleX1, pid)
